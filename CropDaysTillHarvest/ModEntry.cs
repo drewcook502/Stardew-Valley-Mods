54de96bdfef703d7c39b2dc9e12a598958ba14e0
Andrew Cook <drewcook502@gmail.com>
2018-06-03 14:56:55 -0600

    Added support for trees and fruit trees

--- CropDaysTillHarvest/ModEntry.cs
+++ CropDaysTillHarvest/ModEntry.cs
@@ -44,31 +44,44 @@ namespace Tutorial
                return;
             }
 
-            Vector2 location = Game1.currentCursorTile;
-            Farm farm = Game1.getFarm();
-            if (farm.terrainFeatures.TryGetValue(location, out TerrainFeature feature))
+            Vector2 tile = Game1.currentCursorTile;
+            GameLocation curLocation = Game1.currentLocation;
+            
+            if (curLocation.terrainFeatures.TryGetValue(tile, out TerrainFeature feature))
             {
-               HoeDirt dirt = feature as HoeDirt;
-               if (!object.ReferenceEquals(dirt, null) && !object.ReferenceEquals(dirt.crop, null))
+               string message = "";
+               if (feature is HoeDirt dirt)
                {
-                  string message = GetMessage(dirt.crop);
-                  DebugLogMessage(message);
-                  //nothing is an empty window
-                  //1 is achivement
-                  //2 gives a giant ! in the box --new quest
-                  //3 is giant red X -- error
-                  //4 is a giant green +  --stamina
-                  //5 is a giant red +  --health
-
-                  if (!Game1.doesHUDMessageExist(message))
+                  if (!object.ReferenceEquals(dirt.crop, null))
                   {
-                     HUDMessage hudMessage = new HUDMessage(message);
-
-                     hudMessage.noIcon = true;//good idea if I can't figure out how to get the icon of the crop
-                     
-                     Game1.addHUDMessage(hudMessage);
+                     message = GetCropMessage(dirt.crop);
                   }
+               }
+               else if (feature is FruitTree fruitTree)
+               {
+                  message = GetFruitTreeMessage(fruitTree);
+               }
+               else if (feature is Tree tree)
+               {
+                  message = GetTreeMessage(tree);
+               }
 
+
+               DebugLogMessage(message);
+               //nothing is an empty window
+               //1 is achivement
+               //2 gives a giant ! in the box --new quest
+               //3 is giant red X -- error
+               //4 is a giant green +  --stamina
+               //5 is a giant red +  --health
+
+               if (!string.IsNullOrWhiteSpace(message) && !Game1.doesHUDMessageExist(message))
+               {
+                  HUDMessage hudMessage = new HUDMessage(message);
+
+                  hudMessage.noIcon = true;//good idea if I can't figure out how to get the icon of the crop
+
+                  Game1.addHUDMessage(hudMessage);
                }
             }
 
@@ -80,11 +93,13 @@ namespace Tutorial
          }
       }
 
-      private string GetMessage(Crop crop)
+      private string GetCropMessage(Crop crop)
       {
+         string name = GetNameFromIndex(crop.indexOfHarvest.Value);
+
          if (crop.dead.Value)
          {
-            return GetCropName(crop) + " is dead";
+            return name + " is dead";
          }
 
          //get all remaining phases
@@ -112,7 +127,6 @@ namespace Tutorial
          }
          DebugMessage(numOfDaysRemaining >= 0, "numOfDaysRemaining is negative. Look into!");
 
-         string name = GetCropName(crop);
          string message;
          if (numOfDaysRemaining == 0)
          {
@@ -127,11 +141,81 @@ namespace Tutorial
 
       }
 
-      private string GetCropName(Crop crop)
+      private string GetFruitTreeMessage(FruitTree tree)
       {
-         int nameIndex = crop.indexOfHarvest.Value;
+         string message = "";
 
-         Game1.objectInformation.TryGetValue(nameIndex, out string name);
+         int fruitIndex = tree.indexOfFruit.Value;
+
+         message = GetNameFromIndex(fruitIndex) + " tree";
+
+         int growthStage = tree.growthStage.Value;
+
+         int daysUntilMature = tree.daysUntilMature.Value;
+         if (daysUntilMature > 0)
+         {
+            //not fully grown, count days till fully grown
+            message += " has " + daysUntilMature + " until mature";
+         }
+
+         string season = tree.fruitSeason.Value;
+
+         if (!string.IsNullOrWhiteSpace(season))
+         {
+            message += ". Bears fruit in " + season;
+         }
+
+         return message;
+      }
+
+      private string GetTreeMessage(Tree tree)
+      {
+         string message;
+
+         int treeType = tree.treeType.Value;
+         switch (treeType)
+         {
+            case Tree.bushyTree:
+            case Tree.winterTree1:
+            {
+               message = "Oak tree";
+               break;
+            }
+            case Tree.leafyTree:
+            case Tree.winterTree2:
+            {
+               message = "Maple tree";
+               break;
+            }
+            case Tree.pineTree:
+            {
+               message = "Pine tree";
+               break;
+            }
+            case Tree.palmTree:
+            {
+               message = "Palm tree";
+               break;
+            }
+            case Tree.mushroomTree:
+            {
+               message = "Mushroom tree";
+               break;
+            }
+            default:
+            {
+               DebugMessage(false, "Unexpected tree type. Do not know the name.");
+               message = "";
+               break;
+            }
+         }
+
+         return message;
+      }
+
+      private string GetNameFromIndex(int index)
+      {
+         Game1.objectInformation.TryGetValue(index, out string name);
 
          //of course I have to parse it...
          return name.Split('/')[4];
